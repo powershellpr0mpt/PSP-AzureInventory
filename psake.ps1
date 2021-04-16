f63849f21ad3315cd750e08cca2af0c03b020784
Robert Prüst <robert@powershellpr0mpt.com>
2021-04-16 13:45:28 +0200

    updated psake file for clarity

--- psake.ps1
+++ psake.ps1
@@ -5,61 +5,62 @@ Properties {
     $ProjectRoot = $env:BHProjectPath
     $ModuleName = $env:BHProjectName
     $ModuleVersion = (Get-Module -ListAvailable $env:BHPSModuleManifest).Version
-    $TestsFolder = "$ProjectRoot\Tests"
-    $TestsOutputFolder = "$ProjectRoot\_testresults\"
-    $TestsOutput = "$TestsOutputFolder\Test_Help`_$TimeStamp.xml"
     $BuildFolder = "$ProjectRoot\_bin\$ModuleName"
     $VersionFolder = "$BuildFolder\$ModuleVersion"
-    $ZipFolder = "$ProjectRoot\_zip"
 }
 
 Task default -Depends Build
 
 Task Build {
     Write-Host "Building Module Structure"  -ForegroundColor Blue
+
+    # Build folder
+    If (-not (Test-Path $BuildFolder)) {
+        Write-Host "Creating Build Folder"  -ForegroundColor Blue
+        $Null = New-Item -Path $BuildFolder -Type Directory -Force
+    }
+    Else {
+        Write-Host "Clearing Existing Build Folder  $BuildFolder"  -ForegroundColor Blue
+        Remove-Item -Path $BuildFolder/* -Recurse -Force
+    }
+    # Version folder
+    Write-Host "Creating Version Folder"  -ForegroundColor Blue
+    $Null = New-Item -Path $VersionFolder -Type Directory -Force
+
+    <#
     $FunctionsPublic = Get-ChildItem -Path $ProjectRoot\$ModuleName\Public -Recurse -Exclude *.Tests.* -File `
         | ForEach-Object -Process {Get-Content -Path $_.FullName; "`r`n"}
     $FunctionsPrivate = Get-ChildItem -Path $ProjectRoot\$ModuleName\Private -Recurse -Exclude *.Tests.* -File `
         | ForEach-Object -Process {Get-Content -Path $_.FullName; "`r`n"}
+#>
 
-    If (-not (Test-Path $BuildFolder)) {
-        Write-Host "Creating Build Folder"  -ForegroundColor Blue
-        $Null = New-Item -Path $BuildFolder -Type Directory -Force
-    } Else {
-        Write-Host "Clearing Existing Build Folder  $BuildFolder"  -ForegroundColor Blue
-        Remove-Item -Path $BuildFolder/* -Recurse -Force
-    }
-    Write-Host "Creating Version Folder"  -ForegroundColor Blue
-    $Null = New-Item -Path $VersionFolder -Type Directory -Force
-
-    If (-not (Test-Path $ZipFolder)) {
-        Write-Host "Creating Zip Folder"  -ForegroundColor Blue
-        $Null = New-Item -Path $ZipFolder -Type Directory -Force
-    } Else {
-        Write-Host "Clearing Existing Zip Folder  $ZipFolder"  -ForegroundColor Blue
-        Remove-Item -Path $ZipFolder/* -Recurse -Force
-    }
-
-    Write-Host "Copying Module Content to Build folder"  -ForegroundColor Blue
+    # Copy to Version Folder
+    Write-Host "Copying Module Content to Version folder"  -ForegroundColor Blue
     $Null = Copy-Item -Path "$ProjectRoot\$ModuleName\" -Recurse -Destination $VersionFolder -Force
 
-    Write-Host "Update the PSD1 FunctionsToExport for autoloading on build folder"  -ForegroundColor Blue
+    # Update FunctionsToExport in Module Manifest for autoloading
+    Write-Host "Update the Module Manifest FunctionsToExport for autoloading on build"  -ForegroundColor Blue
     Set-ModuleFunctions -Name "$VersionFolder\$ModuleName"
 
-    Write-Host "Update the PSD1 FormatsToExport for applying custom formats on cmdlets" -ForegroundColor Blue
-    Set-ModuleFormats -Name "$VersionFolder\$ModuleName" -FormatsRelativePath '.\formats'
+    # Add custom Formats in Module Manifest when existing
+    if (Get-ChildItem -Path "$VersionFolder\$ModuleName\formats" -File *.format.ps1xml) {
+        Write-Host "Update the Module Manifest FormatsToExport for applying custom formats on cmdlets" -ForegroundColor Blue
+        Set-ModuleFormats -Name "$VersionFolder\$ModuleName" -FormatsRelativePath '.\formats'
+    }
+    
 
+    # Verifying new module
     Write-Host "Module built, verifying module output" -ForegroundColor Blue
     Get-Module -ListAvailable "$VersionFolder\$ModuleName\$ModuleName.psd1" | ForEach-Object -Process {
         $ExportedFunctions = $_ `
-            | Select-Object -Property @{ Name = "ExportedFunctions" ; Expression = { [string[]]$_.ExportedFunctions.Keys } } `
-            | Select-Object -ExpandProperty ExportedFunctions
+        | Select-Object -Property @{ Name = "ExportedFunctions" ; Expression = { [string[]]$_.ExportedFunctions.Keys } } `
+        | Select-Object -ExpandProperty ExportedFunctions
         $ExportedAliases = $_ `
-            | Select-Object -Property @{ Name = "ExportedAliases"   ; Expression = { [string[]]$_.ExportedAliases.Keys   } } `
-            | Select-Object -ExpandProperty ExportedAliases
+        | Select-Object -Property @{ Name = "ExportedAliases"   ; Expression = { [string[]]$_.ExportedAliases.Keys } } `
+        | Select-Object -ExpandProperty ExportedAliases
         $ExportedVariables = $_ `
-            | Select-Object -Property @{ Name = "ExportedVariables" ; Expression = { [string[]]$_.ExportedVariables.Keys } } `
-            | Select-Object -ExpandProperty ExportedVariables
+        | Select-Object -Property @{ Name = "ExportedVariables" ; Expression = { [string[]]$_.ExportedVariables.Keys } } `
+        | Select-Object -ExpandProperty ExportedVariables
         Write-Output "Name              : $($_.Name)"
         Write-Output "Description       : $($_.Description)"
         Write-Output "Guid              : $($_.Guid)"
@@ -80,13 +81,17 @@ Task Analyze -Depends Build {
 }
 
 Task Test -Depends Analyze {
+    $TestsFolder = "$ProjectRoot\Tests"
+    $TestsOutputFolder = "$ProjectRoot\_testresults\"
+    $TestsOutput = "$TestsOutputFolder\Test_Help`_$TimeStamp.xml"
+
     If (-not (Test-Path $TestsOutputFolder)) {
         Write-Host "Creating Tests Output Folder"  -ForegroundColor Blue
         $Null = New-Item -Path $TestsOutputFolder -Type Directory -Force
     }
 
     Write-Host "Removing Test Output > 5 runs ago"
-    Get-ChildItem $TestsOutputFolder -Recurse | Where-Object {-not $_.PSIsContainer} | Sort-Object CreationTime -Descending | Select-Object -Skip 5 | Remove-Item -Force
+    Get-ChildItem $TestsOutputFolder -Recurse | Where-Object { -not $_.PSIsContainer } | Sort-Object CreationTime -Descending | Select-Object -Skip 5 | Remove-Item -Force
 
     Write-Host "Testing Module"  -ForegroundColor Blue
     $HelpResults = Invoke-Pester $TestsFolder -OutputFormat NUnitXml -OutputFile $TestsOutput -PassThru
@@ -96,6 +101,18 @@ Task Test -Depends Analyze {
 }
 
 Task WinZip -depends Test {
+    $ZipFolder = "$ProjectRoot\_zip"
+
+    # Zip folder
+    If (-not (Test-Path $ZipFolder)) {
+        Write-Host "Creating Zip Folder"  -ForegroundColor Blue
+        $Null = New-Item -Path $ZipFolder -Type Directory -Force
+    }
+    Else {
+        Write-Host "Clearing Existing Zip Folder  $ZipFolder"  -ForegroundColor Blue
+        Remove-Item -Path $ZipFolder/* -Recurse -Force
+    }
+
     $FileName = "$ZipFolder\$ModuleName.$ModuleVersion.zip"
     Compress-Archive -Path "$VersionFolder\$ModuleName\" -DestinationPath $FileName -Force
 }
